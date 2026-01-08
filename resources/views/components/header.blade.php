261a76892d00e60781500d4d25093b4cf396a1a9
Smilie Pop <arisatominato48@gmail.com>
2026-01-08 23:03:59 +0800

    refactor: added logo image

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,9 @@
 <header class="box-border flex flex-row items-center justify-between px-8 py-4">
-    <div class="left text-3xl">LingapIskolar</div>
+    <div class="left text-3xl">
+        <a href="{{ route("root") }}">
+            <img src="/img/home-logo.jpg" alt="" class="w-64" />
+        </a>
+    </div>
     <div class="right flex gap-4">
         @auth
             
@@ -7,7 +11,9 @@
 
         @guest
             <x-button :href="route('login')">Log In</x-button>
-            <x-button :href="route('signup')" :variant="'secondary'">Sign Up</x-button>
+            <x-button :href="route('signup')" :variant="'secondary'">
+                Sign Up
+            </x-button>
         @endguest
     </div>
 </header>
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get("/", function () {
     return view("routes.root");
-});
+})->name("root");
 
 Route::get("/login", function () {
     return "login";
