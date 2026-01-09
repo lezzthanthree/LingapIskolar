f89e479825948feb55d55dacebd655b8cb9003db
Smilie Pop <arisatominato48@gmail.com>
2026-01-09 12:37:14 +0800

    feat: added login and signup pages

---
+++ resources/views/components/text-input.blade.php
@@ -0,0 +1,19 @@
+<div class="relative">
+    @if ($icon)
+        <i
+            class="bi {{ $icon }} absolute top-1/2 left-4 -translate-y-1/2 text-4xl text-neutral-800"
+        ></i>
+    @endif
+
+    <input
+        {{
+            $attributes->merge([
+                "class" => $getStyleOfInput(),
+                "id" => $id,
+                "placeholder" => $label,
+                "type" => $type,
+                "name" => $id,
+            ])
+        }}
+    />
+</div>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,12 +4,16 @@
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>LingapIskolar</title>
+        <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css"
+        />
         @vite("resources/css/app.css")
     </head>
     <body class="flex min-h-screen min-w-screen flex-col overflow-x-hidden">
         <x-header />
         <main
-            class="h-full flex-1 p-4"
+            class="flex h-full flex-1 p-4"
             style="
                 background-image: url('{{ asset(auth()->check() ? "/img/auth-bg.png" : "/img/public-bg.png") }}');
             "
